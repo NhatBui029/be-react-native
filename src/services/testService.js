2c7d4f3e7fe5ef2982f0bb89831194e238c594c7
NhatBui029 <buituannhat1402@gmail.com>
2024-04-22 13:58:53 +0700

    'last'

--- src/services/testService.js
+++ src/services/testService.js
@@ -44,27 +44,30 @@ module.exports = {
     addProduct: async () => {
         const res = await prisma.product.create({
             data: {
-                name: 'Giày Nike Air Jordan 1 Low GS Triple White',
-                description: 'Nike Air Jordan 1 với lịch sử hơn 30 năm luôn được nhìn nhận như một trong những dòng sản phẩm thành công nhất của Nike. Nike Jordan 1 luôn bán hết một cách nhanh chóng ngay từ khi ra mắt đến nay, luôn là sản phẩm được các tín đồ thời trang chú ý hàng đầu. Air Jordan được đặt dựa theo ngôi sao bóng rổ lừng danh Michael Jordan – huyền thoại của NBA.',
-                oldPrice: 2350000,
-                newPrice: 1500000,
-                countInStock: 35,
-                sold: 43,
-                rating: 5,
+                name: 'Giày Air Jordan 1 Low WMNS - Sky J Orange',
+                description: 'Luôn luôn trong, luôn tươi mới. Air Jordan 1 Low mang đến cho bạn một phần lịch sử và di sản của Jordan mà bạn cảm thấy thoải mái cả ngày. Chọn màu sắc của bạn, sau đó bước ra ngoài với thiết kế mang tính biểu tượng được xây dựng bằng sự kết hợp giữa các vật liệu cao cấp và Air được gói gọn ở gót chân.',
+                oldPrice: 3999000,
+                newPrice: 3450000,
+                countInStock: 43,
+                sold: 98,
+                rating: 4,
                 images: {
                     create: [
                         {
-                            url: 'https://bizweb.dktcdn.net/100/413/756/products/air-jordan-1-low-shoe-pcxgkc.png?v=1675918105223'
+                            url: 'https://product.hstatic.net/1000361048/product/dc0774_080_c_a1172bb365c145daa311610b1601ad91_master.jpg'
                         }
                     ]
                 },
                 options: {
                     create: [
-                        { name: '36' },
-                        { name: '37' },
-                        { name: '38' },
-                        { name: '39' },
-                        { name: '40' },
+                        { name: '36', countInStock: 7, sold: 10 },
+                        { name: '37',countInStock: 8, sold: 15 },
+                        { name: '38' ,countInStock: 9, sold: 12},
+                        { name: '39' ,countInStock: 5, sold: 16},
+                        { name: '40' ,countInStock: 10, sold: 13},
+                        { name: '41' ,countInStock: 12, sold: 15},
+                        { name: '42' ,countInStock: 3, sold: 18},
+                        { name: '42' ,countInStock: 6, sold: 12},
                     ]
                 }
             },
